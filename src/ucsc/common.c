eba9e64c53d8526e5ed9465b4d0df95d3f80559e
Ge Tan <ge.tan09@imperial.ac.uk>
2014-03-11 18:22:50 +0000

    version 0.99.16. success on windows build
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CNEr@87315 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/ucsc/common.c
+++ src/ucsc/common.c
@@ -3361,6 +3361,8 @@ char *splitOffNumber(char *db)
 return cloneString(skipToNumeric(db));
 }
 
+#ifndef WIN32
+
 time_t mktimeFromUtc (struct tm *t)
 /* Return time_t for tm in UTC (GMT)
  * Useful for stuff like converting to time_t the
@@ -3486,3 +3488,5 @@ if (strptime(date,format, &tp))
     }
 return cloneString(newDate);  // newDate is never freed!
 }
+
+#endif
