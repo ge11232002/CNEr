c056b70799ba10974bc48b5fbab7faa252548822
Ge Tan <gtan@me.com>
2014-05-13 14:39:56 +0100

    DB.R

---
+++ R/DB.R
@@ -0,0 +1,43 @@
+### -----------------------------------------------------------------
+### query the new CNEData data package
+### Not exported!
+#dbName = "/mnt/biggley/projects/gtan/projects/CNEr/CNErData/CNEData.sqlite"
+#target = "equCab2"
+#query = "canFam3"
+#type = "all"
+#winSize = 50
+#identity = 49
+#foo = queryCNEData(dbName, target, query, winSize, identity, type)
+
+queryCNEData <- function(dbName, target, query, winSize, identity,
+                         type=c("target", "query", "all")
+                         ){
+  con <- dbConnect(SQLite(), dbname=dbName)
+  on.exit(dbDisconnect(con))
+  tableName <- ifelse(target < query, paste(target, query, sep="_"),
+                      paste(query, target, sep="_"))
+  tableName <- paste(tableName, identity, winSize, sep="_")
+  if(type == "target"){
+    if(target < query){
+      sqlCmd <- "SELECT chr1, start1, end1"
+    }else{
+      sqlCmd <- "SELECT chr2, start2, end2"
+    }
+  }else if(type == "query"){
+    if(target < query){
+      sqlCmd <- "SELECT chr2, start2, end2"
+    }else{
+      sqlCmd <- "SELECT chr1, start1, end1"
+    }
+  }else if(type == "all"){
+    if(target < query){
+      sqlCmd <- "SELECT chr1, start1, end1, chr2, start2, end2"
+    }else{
+      sqlCmd <- "SELECT chr2, start2, end2, chr1, start1, end1"
+    }
+  }
+  sqlCmd <- paste(sqlCmd, "FROM", tableName)
+  cne <- dbGetQuery(con, sqlCmd)
+  return(cne)
+}
+
