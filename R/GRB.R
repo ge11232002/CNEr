2a7c07866398967036ac94e93e3b1459404fdbb2
Ge Tan <ge.tan09@imperial.ac.uk>
2015-11-06 13:18:48 +0000

    version 1.7.1. Fix vignette and update URL
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CNEr@110392 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/GRB.R
@@ -0,0 +1,37 @@
+
+
+makeGRBs <- function(x, winSize=NULL, genes=NULL, ratio=0.5){
+  # winSize in kb
+  x <- unlist(x)
+  x <- reduce(x)
+  cov <- coverage(x)
+
+  # Guess winSize from genome size if NULL
+  if(is.null(winSize)){
+    ## Based on 300kb for human
+    winSize <- seqlengths(x) / 3e6 * 300
+  }
+  winSize <- winSize * 1e3
+
+  # calculate the background percentage of coverage
+  coveredAll <- sum(sum(cov)) / sum(as.numeric(seqlengths(x)))
+  density <- runmean(cov, k=winSize,  endrule="constant")
+
+  # slice the density into GRBs
+  s <- slice(density, lower=coveredAll*ratio, includeLower=FALSE)
+  clusterRanges <- GRanges(seqnames=rep(names(s), elementLengths(s)),
+                           ranges=unlist(ranges(s)),
+                           strand="+",
+                           seqinfo=seqinfo(x))
+
+  # remove GRBs (which do not encompass any gene)
+  if(!is.null(genes)){
+    hits <- findOverlaps(genes, clusterRanges, type="within", select="all",
+                         ignore.strand=TRUE)
+    indexKeep <- unique(subjectHits(hits))
+    clusterRanges <- clusterRanges[indexKeep]
+  }
+  return(clusterRanges)
+
+}
+
