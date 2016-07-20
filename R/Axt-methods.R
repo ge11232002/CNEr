6cecdf867ffc76149228c797b9ba1bed4022e5ee
Ge Tan <gtan@me.com>
2016-07-20 15:33:07 +0200

    version 1.9.26
    Fix the error caused by ambiguous bases

--- R/Axt-methods.R
+++ R/Axt-methods.R
@@ -7,33 +7,41 @@ setGeneric("matchDistribution", function(x, size=10000)
 
 setMethod("matchDistribution", signature(x="Axt"),
           function(x, size=10000){
-            if(length(x) > size){
-              x <- x[sort(sample.int(length(x), size))]
-            }
-            matchFreq <- table(unlist(mapply(paste0,
-                               strsplit(as.character(targetSeqs(x)), ""), 
-                               strsplit(as.character(querySeqs(x)), ""))))
-            matchFreq <- matchFreq / sum(matchFreq) * 100
-            freqMatrix <- matrix(0, ncol=6, nrow=6)
-            colnames(freqMatrix) <- rownames(freqMatrix) <- 
-              c("A", "C", "G", "T", "-", "N")
-            for(i in 1:length(matchFreq)){
-              splittedNames <- strsplit(names(matchFreq)[i], "")[[1]]
-              freqMatrix[splittedNames[1], splittedNames[2]] <-
-                matchFreq[i]
-            }
-            toPlot <- melt(freqMatrix)
-            colnames(toPlot) <- c("target", "query", "percentage")
-            ggplot(toPlot, aes_string(x="target", y="query", 
-                                      fill="percentage")) +
-              geom_tile() +
-              theme_bw() + xlab("Target") + ylab("Query") +
-              ggtitle("Distribution of matched alignments") +
-              scale_fill_continuous(low="deepskyblue4", high="gold") +
-              geom_text(aes(fill=toPlot$percentage, 
-                            label=round(toPlot$percentage, 4)))
+            axtMatchDistribution(x, size=size)
           }
           )
+
+axtMatchDistribution <- function(x, size=10000){
+  if(length(x) > size){
+    x <- x[sort(sample.int(length(x), size))]
+  }
+  matchFreq <- table(unlist(mapply(paste0,
+                                   strsplit(as.character(targetSeqs(x)), ""), 
+                                   strsplit(as.character(querySeqs(x)), ""))))
+  matchFreq <- matchFreq / sum(matchFreq) * 100
+  freqMatrix <- matrix(0, ncol=6, nrow=6)
+  colnames(freqMatrix) <- rownames(freqMatrix) <- 
+    c("A", "C", "G", "T", "-", "N")
+  for(i in 1:length(matchFreq)){
+    splittedNames <- strsplit(names(matchFreq)[i], "")[[1]]
+    ## Some ambiguous bases are not considered here.
+    if(!all(splittedNames %in% colnames(freqMatrix))){
+      next
+    }
+    freqMatrix[splittedNames[1], splittedNames[2]] <-
+      matchFreq[i]
+  }
+  toPlot <- melt(freqMatrix)
+  colnames(toPlot) <- c("target", "query", "percentage")
+  ggplot(toPlot, aes_string(x="target", y="query", 
+                            fill="percentage")) +
+    geom_tile() +
+    theme_bw() + xlab("Target") + ylab("Query") +
+    ggtitle("Distribution of matched alignments") +
+    scale_fill_continuous(low="deepskyblue4", high="gold") +
+    geom_text(aes(fill=toPlot$percentage, 
+                  label=round(toPlot$percentage, 4)))
+}
 
 ### -----------------------------------------------------------------
 ### summary function for Axt
