5550270a9c3bd3f27f09868de665dd02b46e07ad
Ge Tan <gtan@me.com>
2016-12-01 21:31:42 +0000

    version 1.11.5
    Add the function of makeAxtTracks

--- R/Axt-methods.R
+++ R/Axt-methods.R
@@ -99,3 +99,24 @@ setMethod("syntenicDotplot", signature=(x="Axt"),
                                     secondChrs=secondChrs,
                                     col=col, type=type)
           })
+
+### -----------------------------------------------------------------
+### makeAxtTracks
+### Exported!!
+makeAxtTracks <- function(x){
+  if(!is(x, "Axt")){
+    stop(deparse(substitute(x)), " must be a `Axt`` class!")
+  }
+  x <- fixCoordinates(x)
+  
+  targetAxt <- targetRanges(x)
+  queryAxt <- queryRanges(x)
+  
+  targetAxt$name <- as.character(queryAxt)
+  queryAxt$name <- as.character(targetAxt)
+  
+  export.bed(targetAxt, "targetAxt.bed")
+  export.bed(queryAxt, "queryAxt.bed")
+  
+  invisible(list(targetAxt, queryAxt))
+}
