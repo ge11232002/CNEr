a3a9c44e03da48b4e31717dafa5d3c1aa2471465
Ge Tan <gtan@me.com>
2017-04-22 14:40:25 +0200

    donot export addAncestorGO now

--- tests/testthat/test_GO.R
+++
@@ -1,6 +0,0 @@
-test_that("test_addAncestorGO", {
-  library(GO.db)
-  go <- list(c("GO:0005215", "GO:0006810", "GO:0016020"), "GO:0016579")
-  newGO <- addAncestorGO(go)
-  expect_equal(lengths(newGO), c(8, 17))
-})
